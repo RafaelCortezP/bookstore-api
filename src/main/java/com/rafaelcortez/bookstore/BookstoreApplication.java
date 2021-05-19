e200edc096e646fe1b4d971ddd99fc459234095e
Rafael Cortez <rafaelcortezp@gmail.com>
2021-05-19 18:52:08 -0300

    Repositories and automatic instantiation

--- src/main/java/com/rafaelcortez/bookstore/BookstoreApplication.java
+++ src/main/java/com/rafaelcortez/bookstore/BookstoreApplication.java
@@ -1,13 +1,43 @@
 package com.rafaelcortez.bookstore;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rafaelcortez.bookstore.domain.Categoria;
+import com.rafaelcortez.bookstore.domain.Livro;
+import com.rafaelcortez.bookstore.repositories.CategoriaRepository;
+import com.rafaelcortez.bookstore.repositories.LivroRepository;
+
 @SpringBootApplication
-public class BookstoreApplication {
+public class BookstoreApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private LivroRepository livroRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática", "Livros de TI");
+		
+		Livro l1 = new Livro(null, "Clean code", "Robert Martin", "Lorem ipsum", cat1);
+		
+		cat1.getLivros().addAll(Arrays.asList(l1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1));
+		
+		livroRepository.saveAll(Arrays.asList(l1));		
+		
 	}
 
 }
