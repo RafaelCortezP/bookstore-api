76a2fca08717bb2d9a8b4809a319fe4774c7a083
Rafael Cortez <rafaelcortezp@gmail.com>
2021-05-24 18:37:43 -0300

    Custom exceptions

--- src/main/java/com/rafaelcortez/bookstore/services/CategoriaService.java
+++ src/main/java/com/rafaelcortez/bookstore/services/CategoriaService.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.rafaelcortez.bookstore.domain.Categoria;
 import com.rafaelcortez.bookstore.repositories.CategoriaRepository;
+import com.rafaelcortez.bookstore.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null); 
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto Não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
