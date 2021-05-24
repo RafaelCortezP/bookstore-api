2ade70aae5aa13aa0e8e5c7f3480fddfbeb702cc
Rafael Cortez <rafaelcortezp@gmail.com>
2021-05-24 18:52:32 -0300

    findAll for Categoria

--- src/main/java/com/rafaelcortez/bookstore/services/CategoriaService.java
+++ src/main/java/com/rafaelcortez/bookstore/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.rafaelcortez.bookstore.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,9 @@ public class CategoriaService {
 		Optional<Categoria> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto Não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public List<Categoria> findAll(){
+		return repository.findAll();
 	}
 }
